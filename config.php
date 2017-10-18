5df3384f6e41aacaf22f269ff4bc5fc21565085d
NetoAC <pulpfictionjs@gmail.com>
2017-10-17 22:15:59 -0300

    Alteração nas Pastas

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-try{
-	$pdo = new PDO("mysql:host=localhost;dbname=teste","root","main");
-	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-	echo $e->getMessage();
-}
-?>
--- delete.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include 'config.php';
-$id = $_GET['id'];
-$sql = "DELETE FROM usuarios WHERE id=:id";
-$query = $pdo->prepare($sql);
-$query->execute(array(':id'=>$id));
-?>
