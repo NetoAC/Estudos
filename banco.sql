787e6ed2c5b89c52519179da1621fd40e63f3289
NetoAC <pulpfictionjs@gmail.com>
2017-10-29 21:22:58 -0300

    Organizando git

--- banco.sql
+++
@@ -1,8 +0,0 @@
-create database usuarios(
-	id int auto_increment,
-	nome varchar(45)not null,
-	username varchar(45)not null,
-	email varchar(45)not null,
-	senha varchar(45)not null,
-	primary key(id)
-);
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-	$pdo = new PDO("mysql:host=localhost;dbname=new","root","");
-	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);	
-}catch(PDOException $e){
-	echo $e->getMessage();
-}
-?>
